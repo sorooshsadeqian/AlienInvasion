8e6f22a8992d845cf08a0765582b40265ad85707
Soroosh <sadeqiansoroosh@gmail.com>
2021-07-28 12:56:15 +0430

    Add Alien Class

---
+++ alien.py
@@ -0,0 +1,13 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.image = pygame.image.load("images/alien.bmp")
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.bullet_width
+        self.rect.y = self.rect.bullet_height
