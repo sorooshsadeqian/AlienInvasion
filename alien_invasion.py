c8fd378a4cca2854a5d9999862abf3729c9be76e
Soroosh <sadeqiansoroosh@gmail.com>
2021-07-28 12:21:22 +0430

    Alien Invasion

---
+++ alien_invasion.py
@@ -0,0 +1,71 @@
+import pygame 
+import sys
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+
+class AlienInvasion:
+
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+    
+    def run_game(self):
+        while True:
+            self.check_events()
+            self.ship.update()
+            self._update_bullets()
+            self._update_screen()
+
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.background_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        pygame.display.flip()
+    
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self.fire_bullet()
+        elif event.key == pygame.K_q:
+            sys.exit()
+    
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+    
+    def fire_bullet(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+
+if __name__ == "__main__":
+    alien_invasion = AlienInvasion()
+    alien_invasion.run_game()
